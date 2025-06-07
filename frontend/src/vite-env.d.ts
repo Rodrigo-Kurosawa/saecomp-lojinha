d1d360b125b856f1f465af940b4a40327642686f
Rodrigo <rodrigokurosawa@usp.br>
2025-06-07 20:48:07 -0300

    limpando repositório

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_URL: string
-  // adicione mais variáveis de ambiente aqui se necessário
+  // adicione variáveis de ambiente aqui se necessário
 }
 
 interface ImportMeta {
